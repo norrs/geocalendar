415137c23ee6e830699e0f3f1f6aedb980149c4d
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2011-10-30 02:09:43 +0100

    postfix every url with /

--- geocalendar/geocal/urls.py
+++ geocalendar/geocal/urls.py
@@ -5,9 +5,9 @@ urlpatterns = patterns('geocal.views',
     url(r'^$', 'list_events_year_and_month_links', name='geocal_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'events_month', name='geocal_events_month'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', 'events_day', name='geocal_events_day'),
-    url(r'^details/(?P<entry>\d)$', 'details', name='geocal_details_entry'),
-    url(r'^verify/(?P<entry>\d)$', 'verify_entry', name='geocal_verify_entry'),
-    url(r'^reset/(?P<entry>\d)$', 'reset', name='geocal_reset'),
+    url(r'^details/(?P<entry>\d)/$', 'details', name='geocal_details_entry'),
+    url(r'^verify/(?P<entry>\d)/$', 'verify_entry', name='geocal_verify_entry'),
+    url(r'^reset/(?P<entry>\d)/$', 'reset', name='geocal_reset'),
     url(r'^about/$', 'about', name='geocal_about'),
 
 )
