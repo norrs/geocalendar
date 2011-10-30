5eef5ce0c326625a06b0de63e6792715add08e4b
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2011-10-30 02:16:35 +0100

    allows description to be blank

--- geocalendar/geocal/models.py
+++ geocalendar/geocal/models.py
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class CalendarEntry(models.Model):
     title = models.CharField(_("title"), max_length=100)
-    description = models.TextField(_("description"), help_text=_("Check out <a href='http://daringfireball.net/projects/markdown/basics'>this link</a> for how to use markdown syntax"))
+    description = models.TextField(_("description"), help_text=_("Check out <a href='http://daringfireball.net/projects/markdown/basics'>this link</a> for how to use markdown syntax"), blank=True)
     entry_date = models.DateField(_("entry_date"))
     keyword = models.CharField(_("keyword"), max_length=30)
     picture = FileBrowseField("picture", directory="documents/", max_length=500, extensions=['.jpg', '.jpeg', '.png', '.gif'])
