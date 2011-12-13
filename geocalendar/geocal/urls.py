2369bfb2ccd2edb7d7485c101c5595d9de8d5fbc
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2011-12-13 01:26:47 +0100

    fixed a bug where you couldn't reach the details/verify/reset for events with internal id over 1 digit. Now allow up to 5 digits ;p

--- geocalendar/geocal/urls.py
+++ geocalendar/geocal/urls.py
@@ -6,8 +6,8 @@ urlpatterns = patterns('geocal.views',
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'events_month', name='geocal_events_month'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', 'events_day', name='geocal_events_day'),
     url(r'^details/(?P<entry>\d{1,5})/$', 'details', name='geocal_details_entry'),
-    url(r'^verify/(?P<entry>\d){1,5}/$', 'verify_entry', name='geocal_verify_entry'),
-    url(r'^reset/(?P<entry>\d){1,5}/$', 'reset', name='geocal_reset'),
+    url(r'^verify/(?P<entry>\d{1,5})/$', 'verify_entry', name='geocal_verify_entry'),
+    url(r'^reset/(?P<entry>\d{1,5})/$', 'reset', name='geocal_reset'),
     url(r'^about/$', 'about', name='geocal_about'),
 
 )
